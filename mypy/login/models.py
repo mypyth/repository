a29331a341183711bbe2f8e5bc76de0f8c42d31b
mypyth <Administrator@PC201410231047>
2014-11-07 15:43:16 +0800

    Signed-off-by: mypyth <Administrator@PC201410231047>

--- mypy/login/models.py
+++ mypy/login/models.py
@@ -1,6 +1,13 @@
+#coding=utf-8
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+# 数据库中model的字段
 class User(models.Model):
-    username=models.CharField(max_length=50)
-    password= models.CharField(max_length=50)
+    username=models.CharField(max_length=50) #用户名
+    password= models.CharField(max_length=50) #密码
+    def __unicode__(self): # 字段格式化成字符串格式
+        return self.username
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username","password")
+admin.site.register(User)
--- mypy/login/views.py
+++ mypy/login/views.py
@@ -1,1 +1,48 @@
+#coding=utf-8
 # Create your views here.
+from django.shortcuts import render_to_response
+from login.models import User
+from django.http import HttpResponseRedirect
+from django import forms
+from django.template.context import RequestContext
+
+#定义表单类型
+class UserForm(forms.Form):
+    username = forms.CharField(label='账号：',max_length=100)
+    password = forms.CharField(label='密码：',widget=forms.PasswordInput)
+
+def login(request):
+    if request.method == 'POST':
+        uf = UserForm(request.POST) 
+        if uf.is_valid():
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            user = User.objects.filter(username__exact = username,password__exact=password) 
+            if user:
+                request.session['username'] = username
+                return render_to_response('success.html',{'username':username})
+            else:
+                return HttpResponseRedirect('/login/')
+    else:
+        uf = UserForm()
+    return render_to_response('login.html',{'uf':uf})
+
+def register(request):
+    if request.method =='POST':
+        uf = UserForm(request.POST)
+        if uf.is_valid():
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            user = User.objects.filter(username__exact = username,password__exact=password)
+            if user:
+                return HttpResponseRedirect('/login/register/')
+            else:
+                User.objects.create(username= username,password=password)
+                return render_to_response('registersucc.html',{'username':username})
+    else:
+        uf = UserForm()
+    return render_to_response('register.html',{'uf':uf},context_instance = RequestContext(request)) 
+
+def logout(request):
+    del request.session['username']
+    return HttpResponseRedirect('/login/login/')
